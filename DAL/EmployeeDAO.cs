b703a758ed26818325473b85ac254b19ff7719c5
Sem-Philippo <144238995+Sem-Philippo@users.noreply.github.com>
2024-05-15 16:39:00 +0200

    Tested database connection, added console project for checking code (And created a basedao class that our dao classes will derive from

--- DAL/EmployeeDAO.cs
+++ DAL/EmployeeDAO.cs
@@ -1,7 +1,36 @@
-﻿namespace DAL
-{
-    public class EmployeeDAO
-    {
+﻿using System.Data;
+using Model;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 
+namespace DAL
+{
+    public class EmployeeDAO : BaseDao
+    {
+        public List<Employee> GetAllDrinks()
+        {
+            string query = "SELECT * FROM EMPLOYEE";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            //Don't forget to include sales amount once drink orders are implemented!
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+        private List<Employee> ReadTables(DataTable dataTable)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                employees.Add(CreateEmployeeFromDataRow(dr));
+
+            }
+            return employees;
+        }
+        private Employee CreateEmployeeFromDataRow(DataRow dr)
+        {
+            return new Employee()
+            {
+                FirstName = dr["FirstName"].ToString(),
+            };
+        }
     }
 }
